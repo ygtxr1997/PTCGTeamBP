f3c8f2d59c5d761e6ab813bf65295c4f1ef69543
ygtxr1997 <gavingreenson@icloud.com>
2024-12-19 17:37:32 +0800

    update: font

--- gr_web.py
+++ gr_web.py
@@ -21,7 +21,7 @@ if platform.system() == "Windows":
     default_font = "SimHei"
 else:  # Linux 或其他系统
     default_font = "Noto Sans CJK SC"
-rcParams['font.sans-serif'] = [default_font]
+rcParams['font.sans-serif'] = "SimHei"
 rcParams['axes.unicode_minus'] = False   # 解决负号 "-" 显示为方块的问题
 
 
